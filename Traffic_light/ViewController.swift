7e64deea31ddf3a082951957c34c4df435c57d7a
Yautrose <86356629+Yautrose@users.noreply.github.com>
2022-04-20 21:49:42 +0300

    Initial Commit

---
+++ Traffic_light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Traffic_light
+//
+//  Created by Alexey Artyushenko on 20.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
