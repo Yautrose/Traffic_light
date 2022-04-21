e0df8834b061a477c3a4df5ebd4bc230f026df1d
Yautrose <86356629+Yautrose@users.noreply.github.com>
2022-04-21 12:58:53 +0300

    Modify ViewController.swift

--- Traffic_light/ViewController.swift
+++ Traffic_light/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIImageView!
+    @IBOutlet var yellowLightView: UIImageView!
+    @IBOutlet var greenLightView: UIImageView!
+
+    @IBOutlet var toggleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        redLightView.alpha = 0.3
+        redLightView.layer.cornerRadius = redLightView.frame.height/2
+        //redLightView.layer.borderWidth = 1
+        //redLightView.layer.masksToBounds = false
+        //redLightView.clipsToBounds = true
+        
+        yellowLightView.alpha = 0.3
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.height/2
+        
+        greenLightView.alpha = 0.3
+        greenLightView.layer.cornerRadius = greenLightView.frame.height/2
+        
+        toggleButton.layer.cornerRadius = 10
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func toggleButtonAction() {
+        
+    }
+    
 }
 
